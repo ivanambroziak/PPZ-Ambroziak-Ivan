7a509c1aee7140a399dfe4fd059709aecf8bfffe
ivanambroziak <ivan.v.ambroziak@ukd.edu.ua>
2025-02-20 09:48:41 +0200

    Taks 4 (not completed)

--- Practice_3_Cycles_and_OOP/PR3_Cycles_Classes.py
+++ Practice_3_Cycles_and_OOP/PR3_Cycles_Classes.py
@@ -19,7 +19,7 @@ def task2(multiplier1 = 1, multiplier2 = 11):
         for j in range(multiplier1, multiplier2):
             print(f"{i} × {j} = {i * j}")
 
-# Task 3
+#  Task 3
 #  Створити клас Person. Властивості - name.
 #  Функції - greet. При виклику функції мусить повертатись рядок
 #  “Привіт, мене звуть <ім’я>”.
@@ -38,15 +38,50 @@ class Student(Person):
     def is_student(self):
         print(True)
 
+# Task 4
+# Створити абстрактний базовий клас (ABC) Shape з абстрактним методом area().
+# Створити клас Circle, який успадковує Shape. Властивості - radius.
+# Функції - area(). Створити клас Rectangle, який успадковує Shape.
+# Властивості - width та height. Функції - area()
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        area = 3.14 * self.radius ** 2
+        return area
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        area = self.width * self.height
+        return area
+
+circle1 = Circle(3)
+print(circle1.area())
+rectangle1 = Rectangle(2, 4)
+print(rectangle1.area())
 
 ###
 
-#task1(1,11) # Вивести усі числа від 1 до 10.
-#task1(2, 21,2)  # Вивести усі парні числа від 1 до 20.
-#task1(10, 0, -1) # Вивести усі числа від 10 до 1
+#task1(1,11)    # Вивести усі числа від 1 до 10.
+#task1(2, 21,2)     # Вивести усі парні числа від 1 до 20.
+#task1(10, 0, -1)   # Вивести усі числа від 10 до 1
 
-#task2(1, 100)
+#task2(1, 11)   # Створити таблицю множення від 1 до 10
 
-ivas = Student("Ivas")  # Task 3
-ivas.greet()
-ivas.is_student()
+
+#ivas = Student("Ivas")  # Task 3
+#ivas.greet()
+#ivas.is_student()
